976d6afccbe1809d464399020f73abd9efd599ea
joan <morjoan@amazon.com>
2021-10-04 13:18:03 -0700

    reset state API is now async #1, periodic inlet strategy can now be associated with an optional CW alarm #2

--- source/sample-inlet-strategies/periodic_inlet.py
+++ source/sample-inlet-strategies/periodic_inlet.py
@@ -3,15 +3,18 @@
 
 """
 This module is one of the sample inlet strategies. 
-It increments the serving counter every minute based on an increment_by value provided, but only if the current time is greater than the set start time,
+It increments the serving counter every minute based on an increment_by value provided,
+but only if the current time is greater than the set start time,
 and the end time has not been reached or is set to 0 (indefinite).
+User can optionally attach an alarm to this inlet, and will increment counter
+if the alarm is in an OK state.
 """
 
 import json
 import os
 from urllib.parse import urlparse
 import time
-
+import boto3
 import requests
 from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
 
@@ -21,6 +24,8 @@ INCREMENT_BY = int(os.environ.get("INCREMENT_BY"))
 CORE_API_REGION = os.environ.get("CORE_API_REGION")
 START_TIME = int(os.environ.get("START_TIME"))
 END_TIME = int(os.environ.get("END_TIME"))
+CLOUDWATCH_ALARM = os.environ.get("CLOUDWATCH_ALARM")
+cw_client = boto3.client('cloudwatch')
 
 def lambda_handler(event, context):
     """
@@ -29,14 +34,25 @@ def lambda_handler(event, context):
 
     print(event)
     current_time = int(time.time())
-    if (current_time > START_TIME and (END_TIME == 0 or current_time < END_TIME)):
+    # alarm could be OK, INSUFFICIENT_DATA or ALARM
+    alarm_status = "OK"
+    if CLOUDWATCH_ALARM:
+        response = cw_client.describe_alarms( AlarmNames=[CLOUDWATCH_ALARM])
+        print(response)
+        if "MetricAlarms" in response and response["MetricAlarms"]:
+            alarm_status = response["MetricAlarms"][0]["StateValue"]
+        elif "CompositeAlarms" in response and response["CompositeAlarms"]:
+            alarm_status = response["CompositeAlarms"][0]["StateValue"]
+        else:
+            print("Unable to find alarm.")
+    if (current_time > START_TIME and (END_TIME == 0 or current_time < END_TIME) and alarm_status != "ALARM"):
         core_api = f'{CORE_API_ENDPOINT}/increment_serving_counter'
         body = {
             "event_id": EVENT_ID,
             "increment_by": INCREMENT_BY
         }
         parsed = urlparse(CORE_API_ENDPOINT)
-        print(parsed)
+        #print(parsed)
         # create an authentication signer for AWS
         auth = BotoAWSRequestsAuth(aws_host=parsed.netloc,
                                     aws_region=CORE_API_REGION,
