0504748e2ab3f9dc981c062c59711bd5952555eb
joan <morjoan@amazon.com>
2021-10-19 11:05:30 -0700

    test hitting endpoint

--- deployment/end-workflow-notification.py
+++ deployment/end-workflow-notification.py
@@ -8,15 +8,17 @@ This module sends notification about the end of a workflow run.
 import os
 import json
 import requests
+import json
 from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
 from urllib.parse import urlparse
 
 API_REGION = os.environ.get('AWS_DEFAULT_REGION')
 NOTIFICATION_ENDPOINT = os.environ.get("NOTIFICATION_ENDPOINT")
-PAYLOAD = os.environ.get("PAYLOAD")
+PAYLOAD = json.loads(os.environ.get("PAYLOAD"))
 
 def main():
     print(NOTIFICATION_ENDPOINT)
+    print(PAYLOAD)
     parsed = urlparse(NOTIFICATION_ENDPOINT)
     print(parsed)
     auth = BotoAWSRequestsAuth(aws_host=parsed.netloc,
@@ -24,7 +26,6 @@ def main():
                                aws_service='execute-api')
     response = requests.post(NOTIFICATION_ENDPOINT, json=PAYLOAD, auth=auth, timeout=25)
     print(response)
-    #print(f"/reset_initial_state {response.status_code}")
     print("some stuff")
 
 if __name__ == "__main__":
