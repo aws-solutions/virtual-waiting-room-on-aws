a436bb19c20a45806918d2ff199e7e14d0a41437
joan <morjoan@amazon.com>
2021-10-20 10:08:56 -0700

    test notification

--- deployment/end-workflow-notification.py
+++ deployment/end-workflow-notification.py
@@ -13,20 +13,21 @@ from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
 from urllib.parse import urlparse
 
 API_REGION = os.environ.get('AWS_DEFAULT_REGION')
-NOTIFICATION_ENDPOINT = os.environ.get("NOTIFICATION_ENDPOINT")
-PAYLOAD = json.loads(os.environ.get("PAYLOAD"))
+NOTIFICATION_ENDPOINT = os.environ.get('NOTIFICATION_ENDPOINT')
+PAYLOAD = json.loads(os.environ.get('PAYLOAD'))
+BRANCH = os.environ.get('BRANCH')
+SOLUTION_NAME = 'aws-virtual-waiting-room'
 
 def main():
-    print(NOTIFICATION_ENDPOINT)
     print(PAYLOAD)
+    PAYLOAD['solution_name'] = SOLUTION_NAME
+    PAYLOAD['branch'] = BRANCH
     parsed = urlparse(NOTIFICATION_ENDPOINT)
-    print(parsed)
     auth = BotoAWSRequestsAuth(aws_host=parsed.netloc,
                                aws_region=API_REGION,
                                aws_service='execute-api')
     response = requests.post(NOTIFICATION_ENDPOINT, json=PAYLOAD, auth=auth, timeout=25)
     print(response)
-    print("some stuff")
 
 if __name__ == "__main__":
     main()
