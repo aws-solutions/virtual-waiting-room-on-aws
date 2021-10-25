a41d7f0882a3e03d7791da42cf2d6789d2b075a7
Joan Morgan <33331296+morjoan@users.noreply.github.com>
2021-10-25 09:54:46 -0700

    Update end-workflow-notification.py

--- deployment/end-workflow-notification.py
+++ deployment/end-workflow-notification.py
@@ -15,19 +15,21 @@ from urllib.parse import urlparse
 
 API_REGION = os.environ.get('AWS_DEFAULT_REGION')
 NOTIFICATION_ENDPOINT = os.environ.get('NOTIFICATION_ENDPOINT')
-PAYLOAD = json.loads(os.environ.get('PAYLOAD'))
+
 SOLUTION_NAME = 'aws-virtual-waiting-room'
+PAYLOAD = {}
+PAYLOAD['solution_name'] = SOLUTION_NAME
+PAYLOAD['branch'] = os.environ.get('BRANCH')
+PAYLOAD['workflow_name'] = os.environ.get('WORKFLOW_NAME')
+PAYLOAD['commit_id'] = os.environ.get('COMMIT_ID')
+PAYLOAD['workflow_run_id'] = os.environ.get('WORKFLOW_RUN_ID')
 
 def main():
-    print(PAYLOAD)
-    PAYLOAD['solution_name'] = SOLUTION_NAME
-    PAYLOAD['branch'] = os.environ.get('BRANCH')
-    PAYLOAD['workflow_name'] = os.environ.get('WORKFLOW_NAME')
-    
     parsed = urlparse(NOTIFICATION_ENDPOINT)
     auth = BotoAWSRequestsAuth(aws_host=parsed.netloc,
                                aws_region=API_REGION,
                                aws_service='execute-api')
+    print(PAYLOAD)
     response = requests.post(NOTIFICATION_ENDPOINT, json=PAYLOAD, auth=auth, timeout=25)
     print(response)
 
