ce61e1f20eab63034490b0b3ffea31c3dafe9611
Jim Thario <jtthario@amazon.com>
2021-10-29 14:49:25 -0700

    fix jshint issues

--- source/control-panel/babel.config.js
+++ source/control-panel/babel.config.js
@@ -1,8 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/*jshint esversion: 6 */
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-}
+};
