e9aeb05061687b035a4d87f1ca768ab64def18f2
thyagram-aws <thyagram@amazon.com>
2022-08-21 21:17:53 -0700

    else return

--- source/core-api/lambda_functions/lambda_functions_test.py
+++ source/core-api/lambda_functions/lambda_functions_test.py
@@ -632,8 +632,7 @@ class CoreApiTestCase(unittest.TestCase):
                 _value =  int(mock_redis_cache[key]) + int(value)
                 mock_redis_cache[key] = _value
                 return _value
-            else:
-                return None
+            return None
 
         set_max_queue_position_expired.rc = MagicMock()
         set_max_queue_position_expired.rc.get = Mock(side_effect=mock_get)
