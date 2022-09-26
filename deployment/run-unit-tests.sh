751e0b7275f77416dce781cafc64fdb61cdd0348
thyagram-aws <thyagram@amazon.com>
2022-09-25 21:20:15 -0700

    run unittests and renames

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -12,7 +12,7 @@ template_dir="$PWD"
 source_dir="$template_dir/../source"
 
 echo "------------------------------------------------------------------------------"
-echo " Test core API functions"
+echo " Test core API lambda functions"
 echo "------------------------------------------------------------------------------"
 cd $source_dir/core-api/lambda_functions
 coverage run lambda_functions_tests.py
@@ -26,3 +26,21 @@ cd $source_dir/core-api/custom_resources
 coverage run custom_resource_tests.py
 coverage xml
 sed -i -- 's/filename\=\"/filename\=\"source\/core-api\/custom_resources\//g' coverage.xml
+
+
+echo "------------------------------------------------------------------------------"
+echo " Test shared resources functions"
+echo "------------------------------------------------------------------------------"
+cd $source_dir/shared/
+coverage run shared_resources_tests.py
+coverage xml
+sed -i -- 's/filename\=\"/filename\=\"source\/shared\//g' coverage.xml
+
+
+echo "------------------------------------------------------------------------------"
+echo " Test open-id waitingroom custom resources functions"
+echo "------------------------------------------------------------------------------"
+cd $source_dir/openid-waitingroom/custom_resources
+coverage run custom_resources_tests.py
+coverage xml
+sed -i -- 's/filename\=\"/filename\=\"source\/openid-waitingroom\/custom_resources\//g' coverage.xml
